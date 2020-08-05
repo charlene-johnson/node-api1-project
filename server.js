2c8874a10266ab46b75e9a5184c65a7044d353d0
charlene-johnson <charlene.j8234@gmail.com>
2020-08-05 19:23:24 -0400

    Changed the way I did code, used try catch statements so that I can add the 500 error

--- server.js
+++ server.js
@@ -6,69 +6,99 @@ const server = express();
 
 server.use(express.json());
 
-server.get("/api/users", (req, res) => {
-  const allUsers = db.getUsers();
-  if (allUsers) {
-  res.json(allUsers);
-} else {
-    res.status(500).json({errorMesage: "The users information could not be retrieved."})
-}
-});
-
 server.get("/api/users/:id", (req, res) => {
-    const id = req.params.id
+  try {
+    const id = req.params.id;
 
-    const user = db.getUserById(id)
+    const user = db.getUserById(id);
 
     if (user) {
-        res.status(200).json(user)
-    } else  {
-        res.status(404).json({errorMessage:"The user with the specified ID does not exist."})
+      res.status(200).json(user);
+    } else {
+      res.status(404).json({
+        errorMessage: "The user with the specified ID does not exist.",
+      });
     }
-})
+  } catch (err) {
+    res.status(500).json({
+      errorMessage: "The user information could not be retrieved",
+    });
+  }
+});
+
+server.get("/api/users", (req, res) => {
+  try {
+    const allUsers = db.getUsers();
+    res.json(allUsers);
+  } catch (err) {
+    res.status(500).json({
+      errorMessage: "The user information could not be retrieved",
+    });
+  }
+});
 
 server.post("/api/users", (req, res) => {
-  if (req.body.name === undefined || req.body.bio === undefined) {
-    res.status(400).json({
-      errorMessage: "Please provide name and bio for the user.",
-    });
-  } else {
-    const newUser = db.createUser({
+  try {
+    if (req.body.name === undefined || req.body.bio === undefined) {
+      res.status(400).json({
+        errorMessage: "Please provide name and bio for the user.",
+      });
+    } else {
+      const newUser = db.createUser({
         name: req.body.name,
         bio: req.body.bio,
+      });
+      res.status(201).json(newUser);
+    }
+  } catch (err) {
+    res.status(500).json({
+      errorMessage: "The user information could not be retrieved",
     });
-    res.status(201).json(newUser)
   }
 });
 
 server.delete("/api/users/:id", (req, res) => {
-    const user = db.getUserById(req.params.id)
+  try {
+    const user = db.getUserById(req.params.id);
     if (user) {
-        db.deleteUser(req.params.id)
-        res.status(204).end()
+      db.deleteUser(req.params.id);
+      res.status(204).end();
     } else {
-        res.status(404).json({errorMessage: "The user with the specified ID does not exist."})
+      res.status(404).json({
+        errorMessage: "The user with the specified ID does not exist.",
+      });
     }
-})
+  } catch (err) {
+    res.status(500).json({
+      errorMessage: "The user information could not be retrieved",
+    });
+  }
+});
 
-server.put("/api/users/:id", (req,res) => {
-    const user = db.getUserById(req.params.id)
-    if(req.body.name === undefined || req.body.bio === undefined) {
-        res.status(400).json({
-            errorMessage: "Please provide name and bio for the user.",
-          });
+server.put("/api/users/:id", (req, res) => {
+  try {
+    const user = db.getUserById(req.params.id);
+    if (req.body.name === undefined || req.body.bio === undefined) {
+      res.status(400).json({
+        errorMessage: "Please provide name and bio for the user.",
+      });
     } else if (user) {
-        db.updateUser(req.params.id, req.body)
-        res.status(200).json({
-            name: req.body.name,
-            bio: req.body.bio,
-        })
+      db.updateUser(req.params.id, req.body);
+      res.status(200).json({
+        name: req.body.name,
+        bio: req.body.bio,
+      });
     } else {
-        res.status(404).json({
-            errorMessage: "The user with the specified ID does not exist."
-        })
+      res.status(404).json({
+        errorMessage: "The user with the specified ID does not exist.",
+      });
     }
-})
+  } catch (err) {
+    res.status(500).json({
+      errorMessage: "The user information could not be retrieved",
+    });
+  }
+});
 
 server.listen(8080, () => {
   console.log("The server has started on port 8080, have fun!");
